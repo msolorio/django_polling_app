1f453e26ede6ed3d876e78ff7f2f7fd655ebe8ab
msolorio <msolorio.github@gmail.com>
2020-10-29 23:35:20 -0700

    Adds scaffolding for polls URLConfs and views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return render(request, 'index.html')
+    last_five_questions = Question.objects.order_by('-pub_date')[:5]
+
+    return render(request, './polls/index.html', { 'questions': last_five_questions })
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You\'re looking at the results of {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
+
+
+# def results(request, question_id):
+#     return HttpResponse(f'')
